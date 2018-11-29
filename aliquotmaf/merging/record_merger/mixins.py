05070ca95343feeff10528f662beac8784d6de19
Kyle Hernandez <khernandez@bsd.uchicago.edu>
2018-11-29 00:29:29 +0000

    feat(merged): merged protected

---
+++ aliquotmaf/merging/record_merger/mixins.py
@@ -0,0 +1,46 @@
+"""
+Mixins for `BaseMafRecordMerger` classes
+"""
+from abc import ABCMeta, abstractmethod
+
+class MafMergingAverageColumnsMixin(metaclass=ABCMeta):
+    @abstractmethod
+    def average_columns(self):
+        """
+        :return: a ``tuple`` of column names that should be averaged.
+        """
+
+    def do_mean_to_int(self, vals):
+        """
+        Rounds an array of values to the integer mean.
+
+        :param vals: ``list`` of values to get average
+        :return: the mean as an ``int``
+        """
+        return int('{0:.0f}'.format(sum(vals) / float(len(vals))))
+
+
+class MafMergingCombineColumnsMixin(metaclass=ABCMeta):
+    @abstractmethod
+    def combine_columns(self):
+        """
+        :return: a ``tuple`` of column names that should be combined into
+        a unique set.
+        """
+
+    def do_uniq_list(self, records, column):
+        """
+        Gets a unique list of values.
+
+        :param records: list of `maflib.MafRecord`s to extract data from
+        :param column: the column key to merge
+        :return: unique sorted list of values
+        """
+        vals = []
+        for record in records:
+            curr = record[column].value
+            if isinstance(curr, list):
+                vals.extend(curr)
+            else:
+                vals.append(curr)
+        return sorted(list(set(vals)))
---
+++ aliquotmaf/subcommands/merge_protected/runners/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+from .base import BaseRunner
+from .gdc_1_0_0_merged_protected import GDC_1_0_0_Merged_Protected
+
+__all__ = [
+    BaseRunner,
+    GDC_1_0_0_Merged_Protected
+]
