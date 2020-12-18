ce3fedc0668093d93a6745bff716151686d3709f
Charles Czysz <czysz@uchicago.edu>
2020-12-18 10:18:22 -0600

    Updates to aliquot classes

--- aliquotmaf/runners/vcf_to_aliquot/gdc_1_0_0_aliquot.py
+++ aliquotmaf/runners/vcf_to_aliquot/gdc_1_0_0_aliquot.py
@@ -1,8 +1,10 @@
 """Main vcf2maf logic for spec gdc-1.0.0-aliquot"""
 import urllib.parse
 from operator import itemgetter
+from typing import Optional
 
 import pysam
+from maflib.column_types import MutationStatus
 from maflib.header import MafHeader, MafHeaderRecord
 from maflib.sort_order import BarcodesAndCoordinate
 from maflib.sorter import MafSorter
@@ -28,6 +30,20 @@ from aliquotmaf.subcommands.utils import (
     load_json,
 )
 from aliquotmaf.subcommands.vcf_to_aliquot.runners import BaseRunner
+from aliquotmaf.vcf_to_aliquot.annotators.cosmic import CosmicID
+from aliquotmaf.vcf_to_aliquot.annotators.dbsnp_validation import DbSnpValidation
+from aliquotmaf.vcf_to_aliquot.annotators.hotspot import Hotspot
+from aliquotmaf.vcf_to_aliquot.annotators.nontcga_exac import NonTcgaExac
+from aliquotmaf.vcf_to_aliquot.annotators.reference_context import ReferenceContext
+from aliquotmaf.vcf_to_aliquot.filters import (
+    ExAC,
+    GdcBlacklist,
+    GdcPon,
+    Multiallelic,
+    NonExonic,
+    NormalDepth,
+    OffTarget,
+)
 
 
 class Aliquot:
@@ -41,22 +57,45 @@ class Aliquot:
     def __init__(
         self,
         input_vcf: str,
+        output_maf: str,
         tumor_sample_id: str,
-        normal_sample_id: str = None,
+        tumor_aliquot_uuid: str,
+        tumor_submitter_id: str,
+        reference_fasta_index: str,
+        maf_center: str,
+        sequencer: str,
+        src_vcf_uuid: str,
+        case_uuid: str,
+        normal_sample_id: Optional[str] = None,
+        normal_aliquot_uuid: Optional[str] = None,
+        normal_submitter_id: Optional[str] = None,
         is_tumor_only: bool = False,
     ):
+        """
         assert (normal_sample_id and not is_tumor_only) or (
             not normal_sample_id and is_tumor_only
         ), "Normal_sample_id given when tumor_only True"
+        """
+        self.validate_tumor_only()
 
         self.input_vcf = input_vcf
         self.tumor_sample_id = tumor_sample_id
+        self.tumor_aliquot_uuid = tumor_aliquot_uuid
         self.normal_sample_id = normal_sample_id
+        self.normal_aliquot_uuid = normal_aliquot_uuid
         self.is_tumor_only = is_tumor_only
+        self.reference_fasta_index = reference_fasta_index
+        self.output_maf = output_maf
+        self.tumor_submitter_id = tumor_submitter_id
+        self.maf_center = maf_center
+        self.sequencer = sequencer
+        self.normal_submitter_id = normal_submitter_id
+        self.src_vcf_uuid = src_vcf_uuid
+        self.case_uuid = case_uuid
 
-        self._sorter = None
-        self._header = None
-        self._writer = None
+        self._header: MafHeader = None
+        self._sorter: MafSorter = None
+        self._writer: MafWriter = None
 
         self._scheme = None
         self._columns = None
@@ -80,13 +119,7 @@ class Aliquot:
         """
 
     @property
-    def header(
-        self,
-        reference_fasta_index: str = None,
-        tumor_only: bool = False,
-        tumor_aliquot_uuid: str = None,
-        normal_aliquot_uuid: str = None,
-    ):
+    def header(self) -> MafHeader:
         """
         Sets up the maf header.
         """
@@ -95,47 +128,66 @@ class Aliquot:
                 version=self.VERSION,
                 annotation=self.ANNOTATION,
                 sort_order=BarcodesAndCoordinate(),
-                fasta_index=reference_fasta_index,
+                fasta_index=self.reference_fasta_index,
             )
 
             header_date = BaseRunner.get_header_date()
             header[header_date.key] = header_date
 
-            if tumor_only:
+            if self.is_tumor_only:
                 normal_aliquot = MafHeaderRecord(key="normal.aliquot", value="")
             else:
                 normal_aliquot = MafHeaderRecord(
-                    key="normal.aliquot", value=normal_aliquot_uuid,
+                    key="normal.aliquot", value=self.normal_aliquot_uuid,
                 )
             header[normal_aliquot.key] = normal_aliquot
 
             tumor_aliquot = MafHeaderRecord(
-                key="tumor.aliquot", value=tumor_aliquot_uuid
+                key="tumor.aliquot", value=self.tumor_aliquot_uuid
             )
             header[tumor_aliquot.key] = tumor_aliquot
             self._header = header
         return self._header
 
     @property
-    def sorter(self, reference_fasta_index: str = None):
+    def sorter(self) -> MafSorter:
         if not self._sorter:
             self._sorter = MafSorter(
                 max_objects_in_ram=100000,
                 sort_order_name=BarcodesAndCoordinate.name(),
                 scheme=self.header.scheme(),
-                fasta_index=reference_fasta_index,
+                fasta_index=self.reference_fasta_index,
             )
         return self._sorter
 
     @property
-    def writer(self, output_maf: str = None):
+    def writer(self) -> MafWriter:
         if not self._writer:
             self._writer = MafWriter.from_path(
-                path=output_maf,
-                header=self.maf_header,
+                path=self.output_maf,
+                header=self.header,
                 validation_stringency=ValidationStringency.Strict,
             )
         return self._writer
+
+    @property
+    def scheme(self):
+        if not self._scheme:
+            scheme = self.header.scheme()
+            self._scheme = scheme
+        return self._scheme
+
+    @property
+    def colset(self):
+        if not self._colset:
+            self._colset = set(self.columns)
+        return self._colset
+
+    @property
+    def columns(self):
+        if not self._columns:
+            self._columns = get_columns_from_header(self.header)
+        return self._columns
 
     def setup_annotators(self):
         if self.annotators:
@@ -152,26 +204,17 @@ class Aliquot:
         # Add class finder for filters
         pass
 
-    @property
-    def vcf(self, _pysam=pysam):
-        if not self._vcf:
-            vcf = _pysam.VariantFile(self.input_vcf)
-            self._vcf = vcf
-        return self._vcf
-
     def __enter__(self):
-        self.vcf
-        self.header()
-        self.sorter()
         return self
 
     def __exit__(self, x, y, z):
-        self.vcf.close()
         self.sorter.close()
         self.writer.close()
-        for anno in self.annotators:
-            if self.annotators[anno]:
-                self.annotators[anno].shutdown()
+        for anno in self.annotators.values():
+            anno.shutdown()
+
+    def validate_tumor_only(self):
+        pass
 
 
 class GDC_1_0_0_Aliquot(Aliquot):
@@ -179,65 +222,26 @@ class GDC_1_0_0_Aliquot(Aliquot):
     VERSION = "gdc-1.0.0"
     ANNOTATION = "gdc-1.0.0-aliquot"
 
+    ANNOTATORS = (
+        MutationStatus,
+        ReferenceContext,
+        DbSnpValidation,
+        CosmicID,
+        NonTcgaExac,
+        Hotspot,
+    )
+    FILTERS = (
+        ExAC,
+        GdcBlacklist,
+        NormalDepth,
+        GdcPon,
+        Multiallelic,
+        NonExonic,
+        OffTarget,
+    )
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-        # Iterate over annotators, use getattr() on self
-        self.annotators = (
-            "dbsnp_priority_db",
-            "reference_context",
-            "cosmic_id",
-            "mutation_status",
-            "non_tcga_exac",
-            "hotspots",
-        )
-
-        self.filters = (
-            "common_in_exac",
-            "gdc_blacklist",
-            "normal_depth",
-            "gdc_pon",
-            "multiallelic",
-            "nonexonic",
-            "offtarget",
-        )
-
-        self._vcf = None
-
-        # Annotators
-
-    @classmethod
-    def __validate_options__(cls, options):
-        """Validates the tumor only stuff"""
-        if options.tumor_only:
-            options.normal_vcf_id = None
-        else:
-            if options.normal_aliquot_uuid is None:
-                raise ValueError("--normal_aliquot_uuid is required")
-            if options.normal_submitter_id is None:
-                raise ValueError("--normal_submitter_id is required")
-            if options.normal_bam_uuid is None:
-                raise ValueError("--normal_bam_uuid is required")
-
-    @property
-    def vcf(self):
-        if not self._vcf:
-            vcf = pysam.VariantFile(self.input_vcf)
-
-    def __enter__(self):
-        # Open input and output files
-        pass
-
-    def __exit__(self, x, y, z):
-        # Close file handlers
-        self.vcf.close()
-        sorter.close()
-        if self.maf_writer:
-            self.maf_writer.close()
-        for anno in self.annotators:
-            if self.annotators[anno]:
-                self.annotators[anno].shutdown()
-        pass
 
     def do_work(self):
         """Main wrapper function for running vcf2maf"""
@@ -266,15 +270,17 @@ class GDC_1_0_0_Aliquot(Aliquot):
 
         try:
             # Validate samples
-            # Move to new class
+            # Moved to new VCF class
             tumor_idx = assert_sample_in_header(
                 vcf_object, self.options["tumor_vcf_id"]
             )
+            # Moved to new VCF class
             normal_idx = assert_sample_in_header(
                 vcf_object, self.options["normal_vcf_id"], can_fail=is_tumor_only
             )
 
             # extract annotation from header
+            # Moved to new VCF class
             ann_cols_format, vep_key = extract_annotation_from_header(
                 vcf_object, vep_key="CSQ"
             )
@@ -345,30 +351,16 @@ class GDC_1_0_0_Aliquot(Aliquot):
 
         self.logger.info("Finished")
 
-    # Annotators as properties?
-
-    @property
-    def mutation_status(self):
-        if not self._mutation_status:
-            self._mutation_status = Annotators.MutationStatus.setup(
-                self._scheme, self.options["caller_id"]
-            )
-        return self._mutation_status
-
-    def setup_annotators(self):
+    def setup_annotators(self, args):
         """
         Sets up all annotator classes.
         """
-        self.annotators["mutation_status"] = Annotators.MutationStatus.setup(
-            self._scheme, self.options["caller_id"]
-        )
+        if not self.annotators:
+            self.annotators = {}
+            for Annotator in self.ANNOTATORS:
+                self.annotators[Annotator.__name__] = Annotator.setup(self.scheme, args)
 
-        self.annotators["reference_context"] = Annotators.ReferenceContext.setup(
-            self._scheme,
-            self.options["reference_fasta"],
-            self.options["reference_context_size"],
-        )
-
+        """
         if self.options["dbsnp_priority_db"]:
             self.annotators["dbsnp_priority_db"] = Annotators.DbSnpValidation.setup(
                 self._scheme, self.options["dbsnp_priority_db"]
@@ -388,36 +380,18 @@ class GDC_1_0_0_Aliquot(Aliquot):
             self.annotators["hotspots"] = Annotators.Hotspot.setup(
                 self._scheme, self.options["hotspot_tsv"]
             )
+        """
 
-    def setup_filters(self):
+    def setup_filters(self, args):
         """
         Sets up all filter classes.
         """
-        self.filters["common_in_exac"] = Filters.ExAC.setup(
-            self.options["exac_freq_cutoff"]
-        )
+        if not self.filters:
+            self.filters = {}
+            for Filter in self.FILTERS:
+                filt = Filter.setup(args)
+                if filt:
+                    self.filters[Filter.__name__] = filt
 
-        self.filters["multiallelic"] = Filters.Multiallelic.setup()
 
-        if self.options["gdc_blacklist"]:
-            self.filters["gdc_blacklist"] = Filters.GdcBlacklist.setup(
-                self.options["gdc_blacklist"]
-            )
-
-        if not self.options["tumor_only"]:
-            self.filters["normal_depth"] = Filters.NormalDepth.setup(
-                self.options["min_n_depth"]
-            )
-
-        if self.options["gdc_pon_vcf"]:
-            self.filters["gdc_pon"] = Filters.GdcPon.setup(self.options["gdc_pon_vcf"])
-
-        if self.options["nonexonic_intervals"]:
-            self.filters["nonexonic"] = Filters.NonExonic.setup(
-                self.options["nonexonic_intervals"]
-            )
-
-        if self.options["target_intervals"]:
-            self.filters["off_target"] = Filters.OffTarget.setup(
-                self.options["target_intervals"]
-            )
+# __END__
