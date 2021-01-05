58d3fb7ff76dbfaf22f96289cf6238406ed639bf
Charles Czysz <czysz@uchicago.edu>
2021-01-05 12:39:39 -0600

    Remove logger

--- aliquotmaf/vcf_to_aliquot/annotators/annotator.py
+++ aliquotmaf/vcf_to_aliquot/annotators/annotator.py
@@ -3,15 +3,12 @@ Base class for all annotators.
 """
 from abc import ABCMeta, abstractmethod
 
-from aliquotmaf.logger import Logger
-
 
 class Annotator(metaclass=ABCMeta):
     def __init__(self, name=None, source=None, scheme=None):
         self.name = None
         self.source = source
         self.scheme = scheme
-        self.logger = Logger.get_logger(self.__class__.__name__)
 
     @classmethod
     @abstractmethod
