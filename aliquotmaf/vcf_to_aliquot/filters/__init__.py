1fff7fb3f7e379ca79de8b157b7462f85417fec5
Charles Czysz <czysz@uchicago.edu>
2021-01-06 09:30:41 -0600

    Remove imports

--- aliquotmaf/vcf_to_aliquot/filters/__init__.py
+++
@@ -1,9 +0,0 @@
-from aliquotmaf.filters.exac import ExAC
-from aliquotmaf.filters.gdc_blacklist import GdcBlacklist
-from aliquotmaf.filters.gdc_pon import GdcPon
-from aliquotmaf.filters.multiallelic import Multiallelic
-from aliquotmaf.filters.nonexonic import NonExonic
-from aliquotmaf.filters.normal_depth import NormalDepth
-from aliquotmaf.filters.offtarget import OffTarget
-
-__all__ = [ExAC, GdcBlacklist, NormalDepth, GdcPon, Multiallelic, NonExonic, OffTarget]
