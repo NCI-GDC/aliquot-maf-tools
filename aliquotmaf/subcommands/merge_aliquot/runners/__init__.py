587aa4b70d311c7d0ab8e15f39ec7f85fba5741b
Kyle Hernandez <khernandez@bsd.uchicago.edu>
2021-08-04 01:04:14 +0000

    feat(BINF-522): add temp fix to allow rna columns without the annotator being handled yet

--- aliquotmaf/subcommands/merge_aliquot/runners/__init__.py
+++ aliquotmaf/subcommands/merge_aliquot/runners/__init__.py
@@ -2,5 +2,6 @@ from __future__ import absolute_import
 
 from .base import BaseRunner
 from .gdc_1_0_0_aliquot_merged import GDC_1_0_0_Aliquot_Merged
+from .gdc_2_0_0_aliquot_merged import GDC_2_0_0_Aliquot_Merged
 
-__all__ = [BaseRunner, GDC_1_0_0_Aliquot_Merged]
+__all__ = [BaseRunner, GDC_1_0_0_Aliquot_Merged, GDC_1_0_0_Aliquot_Merged]
