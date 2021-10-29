8592fc4fef431276026d037ecab00f75e6b0a505
Charles Czysz <czysz@uchicago.edu>
2021-10-29 09:56:38 -0500

    Update base subcommand

--- aliquotmaf/subcommands/base.py
+++ aliquotmaf/subcommands/base.py
@@ -2,32 +2,35 @@
 Abstract base class for all subcommands in aliquot-maf-tool.
 """
 from abc import ABCMeta, abstractmethod
+from typing import TYPE_CHECKING, Protocol
+
+if TYPE_CHECKING:
+    from argparse import ArgumentParser, _SubParsersAction
 
 
-class Subcommand(metaclass=ABCMeta):
+class Subcommand(Protocol):
     @classmethod
     @abstractmethod
-    def __add_arguments__(cls, parser):
+    def __add_arguments__(cls, parser: 'ArgumentParser') -> None:
         """Add the arguments to the parser"""
 
     @classmethod
-    def __get_description__(cls):
+    def __get_description__(cls) -> str:
         """
         Optionally returns description
         """
-        return None
 
     @classmethod
     @abstractmethod
-    def __tool_name__(cls):
+    def __tool_name__(cls) -> str:
         """
         Tool name to use for the subparser
         """
 
     @classmethod
-    def add(cls, subparsers):
+    def add(cls, subparsers: '_SubParsersAction') -> 'ArgumentParser':
         """Adds the given subcommand to the subprsers."""
-        subparser = subparsers.add_parser(
+        subparser: 'ArgumentParser' = subparsers.add_parser(
             name=cls.__tool_name__(), description=cls.__get_description__()
         )
 
