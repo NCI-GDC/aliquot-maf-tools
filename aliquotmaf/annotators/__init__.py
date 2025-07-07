2a257ae93eb6c327637462f7397394d952e7eac6
Tzintzuni Garcia <tzintzuni@uchicago.edu>
2025-07-07 21:06:03 +0000

    removed unused pandas-based GnomAD annotator code

--- aliquotmaf/annotators/__init__.py
+++ aliquotmaf/annotators/__init__.py
@@ -3,8 +3,6 @@ from __future__ import absolute_import
 from .cosmic import CosmicID
 from .dbsnp_validation import DbSnpValidation
 from .entrez import Entrez
-
-# from .gnomad import GnomAD
 from .gnomad_vcf import GnomAD_VCF
 from .hotspot import Hotspot
 from .mutation_status import MutationStatus
@@ -19,6 +17,5 @@ __all__ = [
     NonTcgaExac,
     Hotspot,
     Entrez,
-    # GnomAD,
     GnomAD_VCF,
 ]
